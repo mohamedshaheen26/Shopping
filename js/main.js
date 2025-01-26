242c493ba0b3803268ceea8a98059d5266b5513f
Mohamed Shahin <mshahin@innotech.com.eg>
2025-01-26 15:55:25 +0200

    Update UI

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const cartIcon = document.querySelector("#cart-icon");
+const cartContent = document.querySelector(".cart-content");
 const cart = document.querySelector(".cart");
 const closeCart = document.querySelector("#cart-close");
+
+document.querySelector(".cart-content").innerHTML =
+  "<p class='text-center mb-0'>Please log in to view your cart.</p>";
 
 cartIcon.addEventListener("click", () => {
   cart.classList.add("active");
@@ -8,4 +12,16 @@ cartIcon.addEventListener("click", () => {
 
 closeCart.addEventListener("click", () => {
   cart.classList.remove("active");
+});
+
+document.addEventListener("click", (event) => {
+  if (!cart.contains(event.target) && !cartIcon.contains(event.target)) {
+    cart.classList.remove("active");
+  }
+});
+
+// Logout logic
+document.getElementById("logoutButton").addEventListener("click", () => {
+  localStorage.removeItem("userId");
+  window.location.href = "login.html";
 });
